f8928a7f3e46b812b6805026e00ec6712ae68967
pennypesh <pennydehoney@yahoo.com>
2019-07-06 09:28:36 -0500

    new changes

--- src/main/java/org/launchcode/models/Category.java
+++ src/main/java/org/launchcode/models/Category.java
@@ -1,10 +1,10 @@
 package org.launchcode.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Category {
@@ -16,18 +16,25 @@ public class Category {
     @Size(min=3, max=15)
     private String name;
 
+    @OneToMany
+    @JoinColumn(name = "category_id")
+    private List<Cheese> cheeses = new ArrayList<>();
 
     public Category(){
 
     }
 
     public Category(String name){
-        this.name = name;
-
+        this.name =name;
     }
 
-    public int getId() {
-        return id;
+
+    public List<Cheese> getCheeses() {
+        return cheeses;
+    }
+
+    public void setCheeses(List<Cheese> cheeses) {
+        this.cheeses = cheeses;
     }
 
     public String getName() {
@@ -36,5 +43,9 @@ public class Category {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getId() {
+        return id;
     }
 }
